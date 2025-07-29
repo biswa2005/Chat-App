021e4501c348467bbf8ab85c4c98d91471fa9623
Biswarup Biswas <bbiswasrup2005@gmail.com>
2025-07-30 00:29:16 +0530

    second

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "../src/lib/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
-import authRoutes from "./routes/auth.routes.js";
-
 const app = express();
+app.use(express.json());    
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { updateProfile } from "../controllers/user.controllers.js";
+
 
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.get("/login", login);
-
 router.get("/logout", logout);
+router.put('./update', authMiddleware, updateProfile);
 
 export default router;
